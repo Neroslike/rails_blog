7a76299c89df275ed9747c254ab319d493fe292d
Neroslike <obduvi22@hotmail.com>
2022-07-11 22:46:45 -0400

    add file uploading support add styles to the index page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
+  has_one_attached :picture
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
