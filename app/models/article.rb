599e3afb99523be6c930d748ad82ba593c39a616
Neroslike <obduvi22@hotmail.com>
2022-07-13 17:05:13 -0400

    Remove images feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  has_one_attached :picture
+  has_one_attached :picture, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
